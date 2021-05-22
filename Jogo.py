467da8c372cab2fbdc3658987d028d8353f7693a
Thiago Santos <tynamospark@gmail.com>
2021-05-22 15:26:25 -0300

    Comentários, música e novo tamanho de tela

--- Jogo.py
+++ Jogo.py
@@ -4,32 +4,36 @@ import pygame
 pygame.init()
 
 
-
-WIDTH = 1280    
-HEIGHT = 720
+#Gera a tela
+WIDTH = 720   
+HEIGHT = 480
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Guitarzao')
 
-
-button_width = 150
-button_height = 120
+#Imagens Botão
+button_width = 65
+button_height = 52
 button_img = pygame.image.load('assets/azul.png').convert_alpha()
 button_img_small = pygame.transform.scale(button_img, (button_width, button_height))
 
-firstsong = pygame.mixer.Sound('assets/musica.mp3')
 
-
-button_x = 0
-button_y = 300
+button_x = -button_width
+button_y = 240
 button_yspeed = 3
-button_xspeed = 3
+button_xspeed = 5
 
+#MSons
+firstsong = pygame.mixer.Sound('assets/musica.mp3')
+pygame.mixer.music.load('assets/musica.mp3')
+pygame.mixer.music.set_volume(0.2)
 
 game = True
 
 clock = pygame.time.Clock()
 FPS = 60
 
+#Loop principal
+pygame.mixer.music.play(loops=-1)
 while game:
     clock.tick(FPS)
     for event in pygame.event.get():
@@ -37,8 +41,8 @@ while game:
         if event.type == pygame.QUIT:
             game = False
     button_x +=button_xspeed
-    if button_x >=400:
-        button_x = 0
+    if button_x >=360-button_width:
+        button_x = -button_width
     window.fill((0, 0, 255))
     window.blit(button_img_small, (button_x,button_y))
     pygame.display.update()
